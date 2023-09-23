68ddd726723f58d994688d5b94f2826c327dae68
KoJaeHyeong(고재형) <kojae2423@mogoscorp.com>
2023-09-23 21:05:05 +0900

    :sparkles: Basic_cats_project

---
+++ 01_basic_project/cats_project/src/main.ts
@@ -0,0 +1,13 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
+  await app.listen(PORT);
+
+  logger.log(`http://localhost:${PORT}`, 'ServerPort');
+}
+bootstrap();
